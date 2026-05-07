78d445af3e121e6bf8e2e4b37e76fba87db819f2
Tushar <tushargupta8160604@gmail.com>
2026-05-08 04:51:54 +0530

    small update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     
 
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user && user.email.endsWith('@gmail.com')) {
+      if (user && user.email.endsWith('')) {
         localStorage.setItem('userEmail', user.email);
         localStorage.setItem('userName', user.displayName); 
         setIsLoggedIn(true);
